f78a45eeb950d06e8f08f85bfe18a21988f96290
jtfl2008 <jtfl2008@gmail.com>
2018-11-21 22:48:49 +0800

    配置egg-mongoose

--- config/config.default.js
+++ config/config.default.js
@@ -1,13 +1,18 @@
 'use strict';
 
 module.exports = appInfo => {
-  const config = exports = {};
+  const config = (exports = {});
 
   // use for cookie sign key, should change to your own and keep security
   config.keys = appInfo.name + '_1542809124010_8878';
 
   // add your config here
   config.middleware = [];
-
+  config.mongoose = {
+    client: {
+      url: 'mongodb://127.0.0.1/blog',
+      options: {},
+    },
+  };
   return config;
 };
