ee7db0f9cbf41d95fb5846777a2871a898f684ef
jtfl2008 <jtfl2008@gmail.com>
2018-11-21 22:34:54 +0800

    路由拆分

--- app/router.js
+++ app/router.js
@@ -1,28 +1,20 @@
 'use strict';
-
+const userRouter = require('./routers/user');
+const messageRouter = require('./routers/message');
+const searchRouter = require('./routers/search');
+const homeRouter = require('./routers/home');
 /**
  * @param {Egg.Application} app - egg application
  */
 module.exports = app => {
   const { router, controller } = app;
   router.get('/', controller.home.index);
-
   // User
-  router.get('/users', contoller.user.findAll);
-  router.get('/user/:id', controller.user.findOne);
-  router.post('/user', controller.user.add);
-  router.put('/user/:id', controller.user.update);
-  router.del('/user/:id', controller.user.delete);
-
+  userRouter(app);
   // Message
-  router.get('/messages/:userId', controller.message.findByUserId);
-  router.post('/message', controller.message.add);
-  router.del('/messages/:id', controller.message.delete);
-
+  messageRouter(app);
   // Find
-  router.get('/search/:keyword', controller.seache.find);
-
+  searchRouter(app);
   // Home
-  router.get('/hot', controller.home.findHot);
-  router.get('heart', controller.home.findHeart);
+  homeRouter(app);
 };
---
+++ app/routers/home.js
@@ -0,0 +1,5 @@
+module.exports = app => {
+  const { router, controller } = app;
+  router.get('/hot', controller.home.findHot);
+  router.get('heart', controller.home.findHeart);
+};
