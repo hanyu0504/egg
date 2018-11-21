ad24d7d3885fde4a71b54bb5153033629e7843f8
jtfl2008 <jtfl2008@gmail.com>
2018-11-21 22:23:13 +0800

    路由添加

--- app/router.js
+++ app/router.js
@@ -6,4 +6,23 @@
 module.exports = app => {
   const { router, controller } = app;
   router.get('/', controller.home.index);
+
+  // User
+  router.get('/users', contoller.user.findAll);
+  router.get('/user/:id', controller.user.findOne);
+  router.post('/user', controller.user.add);
+  router.put('/user/:id', controller.user.update);
+  router.del('/user/:id', controller.user.delete);
+
+  // Message
+  router.get('/messages/:userId', controller.message.findByUserId);
+  router.post('/message', controller.message.add);
+  router.del('/messages/:id', controller.message.delete);
+
+  // Find
+  router.get('/search/:keyword', controller.seache.find);
+
+  // Home
+  router.get('/hot', controller.home.findHot);
+  router.get('heart', controller.home.findHeart);
 };
