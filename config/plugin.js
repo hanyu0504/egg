61bf189439db2de4ba6d77e038b10f1abea1d1c6
jtfl2008 <jtfl2008@gmail.com>
2018-12-05 22:39:00 +0800

    添加 cors,egg-mongoose插件

--- config/plugin.js
+++ config/plugin.js
@@ -2,3 +2,13 @@
 
 // had enabled by egg
 // exports.static = true;
+module.exports = {
+  cors: {
+    enable: true,
+    package: 'egg-cors',
+  },
+  mongoose: {
+    enable: true,
+    package: 'egg-mongoose',
+  },
+};
