c1beb97f84932d0424612760a510de3de3005d75
jtfl2008 <jtfl2008@gmail.com>
2018-12-05 22:39:31 +0800

    配置cors，egg-mongoose

--- config/config.default.js
+++ config/config.default.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports = appInfo => {
-  const config = exports = {};
+  const config = (exports = {});
 
   // use for cookie sign key, should change to your own and keep security
   config.keys = appInfo.name + '_1542809124010_8878';
@@ -9,5 +9,22 @@ module.exports = appInfo => {
   // add your config here
   config.middleware = [];
 
+  config.security = {
+    csrf: {
+      enable: false,
+      ignoreJSON: false,
+    },
+    domainWhiteList: [ '*' ],
+  };
+  config.cors = {
+    origin: '*',
+    allowMethods: 'GET,HEAD,PUT,POST,DELETE,PATCH',
+  };
+  config.mongoose = {
+    client: {
+      url: 'mongodb://localhost:27017/blog',
+      options: {},
+    },
+  };
   return config;
 };
